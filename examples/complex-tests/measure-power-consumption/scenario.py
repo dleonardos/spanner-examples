a7cbf246e22166d2367f184ac249c919ee176f61
AndreasPapandreou <30341027+AndreasPapandreou@users.noreply.github.com>
2018-09-19 11:45:38 +0300

    Update scenario.py

--- examples/complex-tests/measure-power-consumption/scenario.py
+++ examples/complex-tests/measure-power-consumption/scenario.py
@@ -25,7 +25,7 @@ import time
 from Spanner import Spanner
 from Testboard import Testboard
 
-testboard = Testboard("tester_demo")
+testboard = Testboard("testboard_name")
 
 # Our Testboard's D3 Pin is connected to a power switching circuit that controls
 # the power going to the device. When we toggle it HIGH, the device will be
